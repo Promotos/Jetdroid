e9fc40aad1baf50249f6f612465db7e7d14a11df
Promotos <promotos@gmx.de>
2018-08-17 23:03:11 +0200

    bugfix

--- Jetdroid/Assets/Jetdroid/Scripts/AlienB.cs
+++ Jetdroid/Assets/Jetdroid/Scripts/AlienB.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AlienB : MonoBehaviour {
 
     private Animator animator;
-    private bool readyToAttack;
+    private bool readyToAttack = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,27 +14,19 @@ public class AlienB : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D target)
     {
-        if (target.gameObject.tag == "Player")
+        if (readyToAttack && target.gameObject.tag == "Player")
         {
-            if (readyToAttack)
-            {
-                var explode = target.GetComponent<Explode>();
-                explode.OnExplode();
-            } else
-            {
-                animator.SetInteger("AnimState", 1);
-            }
+            target.GetComponent<Explode>().OnExplode();
         }
-    }
-
-    void OnTriggerExit2D(Collider2D target)
-    {
-        readyToAttack = false;
-        animator.SetInteger("AnimState", 0);
     }
 
     void Attack()
     {
         readyToAttack = true;
+    }
+
+    void Sleep()
+    {
+        readyToAttack = false;
     }
 }
